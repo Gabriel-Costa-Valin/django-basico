9cd52fb3654826a07a2a351bb3d3bb2ee896c03e
Gabriel Valin <gabriel.cvalin@gmail.com>
2024-02-03 23:56:39 -0300

    Aprendendo mais de django

--- produtos/views.py
+++ produtos/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pessoa
 
 # Create your views here.
 
 def ver_produto(request):
-    nome = 'Gabriel'
-    return render(request, 'ver_produto.html', {'nome': nome})
+    if request.method == 'GET':
+        nome = 'Gabriel'
+        return render(request, 'ver_produto.html', {'nome': nome})
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        idade = request.POST.get('age')
+
+        #pessoas = Pessoa.objects.all()
+        #pessoas = Pessoa.objects.filter(nome=nome)
+        #pessoas.exists()
+        pessoa = Pessoa(nome=nome, idade=idade)
+        pessoa.save()
+        
+        return HttpResponse('Dados enviados com sucesso!')
 
 
 def inserir_produto(request):
